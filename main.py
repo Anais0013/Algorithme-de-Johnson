3d3eb0598b15db163d0c5e0a5a6a81092539aed3
Anais0013 <35455501+Anais0013@users.noreply.github.com>
2020-04-19 16:18:10 +0200

    pas fonctionnel

--- main.py
+++ main.py
@@ -6,33 +6,64 @@
 #Date de derniere modification : le 18/04/20
 #########################################################
 
-list_debut =[][];
-list_fin =[][];
+list_debut =[];
+list_fin =[];
 
-list_machine1 =[487, 870, 20][1, 2, 3];
-list_machine2 =[456, 700, 200][1, 2, 3];
+list_machine1 =[50, 150, 80, 200, 30];
+list_machine2 = [60, 50, 150, 70, 200];
+list_machine1_copy =list(list_machine1);
+list_machine2_copy =list(list_machine2);
 
-list_de_travail =[][];
+list_de_travail =[];
 
-list_final =[][];
+list_final_tache =[1,2,3];
+list_final_ordre =[];
 
 list_de_travail = list_machine1 + list_machine2;
 
 print (list_de_travail);
+#print (list_de_travail.index(870));
 
-while len(list_de_travail) != 0:
+while list_de_travail != []:
+    print(len(list_machine1));
     a = min (list_de_travail);
     if (a in list_machine1):
+        print("debg 1");
+        print(a);
+        b = list_machine1.index(a);
+        c = list_machine1_copy.index(a);
+        print(b);
+        list_fin.append (c+1);
+        print(list_fin);
         list_de_travail.remove (a);
-        list_debut.append (a);
+        del list_machine2[b];
+        del list_machine1[b];
+        print(list_machine1);
+        print(list_machine2);
+        list_de_travail = list_machine1 + list_machine2;
+        print(list_de_travail);
     else:
+        print("debg 2");
+        print(a);
+        b = list_machine2.index(a);
+        c = list_machine2_copy.index(a);
+        print(b);
+        list_fin.append (c+1);
+        print(list_fin);
         list_de_travail.remove (a);
-        list_fin.append (a);
-    print (list_de_travail);
+        del list_machine1[b];
+        del list_machine2[b];
+        print(list_machine1);
+        print(list_machine2);
+        list_de_travail = list_machine1 + list_machine2;
+        print(list_de_travail);
     
 #inverse la liste debut
 list_fin.reverse();
 
 #concatenation
-list_final = list_debut + list_fin;
-print (list_final);
+print(list_debut);
+print(list_fin);
+list_final_ordre = list_debut + list_fin;
+print ("tâche : ",list_final_tache);
+print ("ordre : ",list_final_ordre);
