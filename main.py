34fea6d82fdfc10b2393c8a5151c4f7fdcde5a75
Anais0013 <35455501+Anais0013@users.noreply.github.com>
2020-04-19 16:50:59 +0200

    gere probleme occurence

--- main.py
+++ main.py
@@ -9,10 +9,11 @@
 list_debut =[];
 list_fin =[];
 
-list_machine1 =[50, 150, 80, 200, 30];
-list_machine2 = [60, 50, 150, 70, 200];
+list_machine1 =[80, 40, 60, 50, 60, 50, 80];
+list_machine2 = [60, 30, 60, 80, 60, 50, 50];
 list_machine1_copy =list(list_machine1);
 list_machine2_copy =list(list_machine2);
+list_memory = [];
 
 list_de_travail =[];
 
@@ -24,32 +25,42 @@ list_de_travail = list_machine1 + list_machine2;
 print (list_de_travail);
 
 while list_de_travail != []:
-    a = min (list_de_travail);
-    if (a in list_machine1):
-        print("debg 1");
-        print(a);
-        b = list_machine1.index(a);
-        print(b);
-        c = list_machine1_copy.index(a);
-        print(c);
-        list_debut.append (c+1);
-        print(list_debut);
-        list_de_travail.remove (a);
-        del list_machine2[b];
-        del list_machine1[b];
-        print(list_machine1);
-        print(list_machine2);
-        list_de_travail = list_machine1 + list_machine2;
-        print(list_de_travail);
+    minimum = min (list_de_travail);
+    # garde en memoire les elements traites
+    list_memory.append(a);
+    if (minimum in list_machine1):
+        print("debug 1");
+        print("min");
+        print(minimum);
+        index_minimum_list_machine1 = list_machine1.index(minimum);
+        print(index_minimum_list_machine1);
+        # gere le probleme de doublon
+        if (minimum in list_memory):
+            # compte occurence
+            combien_de_fois_min = list_memory.count(minimum);
+            # on prend la n occurence
+            y=x.index(line)
+            c = list_machine1_copy.index(a);
+            print(c);
+        else :
+            c = list_machine1_copy.index(a);
+            print(c);
+            list_debut.append (c+1);
+            print(list_debut);
+            del list_machine2[b];
+            del list_machine1[b];
+            print(list_machine1);
+            print(list_machine2);
+            list_de_travail = list_machine1 + list_machine2;
+            print(list_de_travail);
     else:
-        print("debg 2");
+        print("debug 2");
         print(a);
         b = list_machine2.index(a);
         c = list_machine2_copy.index(a);
         print(b);
         list_fin.append (c+1);
         print(list_fin);
-        list_de_travail.remove (a);
         del list_machine1[b];
         del list_machine2[b];
         print(list_machine1);
