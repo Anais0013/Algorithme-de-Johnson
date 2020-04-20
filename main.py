b7b163d854b30bf70921031acb6d6096f4c8d87f
Anais0013 <35455501+Anais0013@users.noreply.github.com>
2020-04-20 17:53:37 +0200

    fonctionne

--- main.py
+++ main.py
@@ -1,54 +1,47 @@
 """
 Realise par : GIANNUZZI Anais
 Fichier : main.c
-Resume : algorithme simple de Johnson, ne gère pas les doublons
+Resume : algorithme de Johnson pour l'ordonnancement minimal
 Date de creation : le 18/04/20
-Date de derniere modification : le 19/04/20
-
-Inspiré de : http://www.unit.eu/cours/EnsROtice/module_avance_thg_voo6/co/johnson.html?fbclid=IwAR1WtxnCjKfx0pvMbqT1PVPlEFpbWpQ_-MHZwBo89Ym9NoNivtP67RzV3TI
+Date de derniere modification : le 20/04/20
 """
 
 L1 = [];
 L2 = [];
 
 # Saisie des données
-list_machine1 = [21, 19, 18, 20];
-list_machine2 = [60, 30, 3, 10];
+list_machine1 = [[4, 8, 6, 6, 5, 7], [1, 2, 3, 4, 5, 6]];
+list_machine2 = [[3, 6, 6, 6, 8, 4], [1, 2, 3, 4, 5, 6]];
 
-list_machine1_copy = list(list_machine1);
-list_machine2_copy = list(list_machine2);
+W = list_machine1[0] + list_machine2[0];
 
-W = list_machine1 + list_machine2;
-
-list_final_tache = [1, 2, 3];
+list_final_tache = [1, 2, 3, 4, 5, 6];
 list_final_ordre = [];
 
-while len(list_machine1) != 0:
+while W != []:
     minimum = min(W);
-    if (minimum in list_machine1) :
-        index = list_machine1.index(minimum);
-        index_copy = list_machine1_copy.index(minimum);
-        L1.append(index_copy+1);
+    if (minimum in list_machine1[0]) :
+        index = list_machine1[0].index(minimum);
+        L1.append(list_machine1[1][index]);
         # on supprime la tache
-        del list_machine1[index];
-        del list_machine2[index];
-        W = list_machine1 + list_machine2;
+        del list_machine1[0][index];
+        del list_machine2[0][index];
+        del list_machine1[1][index];
+        del list_machine2[1][index];
+        W = list_machine1[0] + list_machine2[0];
     else :
-        index = list_machine2.index(minimum);
-        index_copy = list_machine2_copy.index(minimum);
-        L2.insert(0, list_machine2_copy.index(minimum)+1);
+        index = list_machine2[0].index(minimum);
+        L2.insert(0, list_machine2[1][index]);
         # on supprime la tache
-        del list_machine1[index];
-        del list_machine2[index];
-        W = list_machine1 + list_machine2;
+        del list_machine1[0][index];
+        del list_machine2[0][index];
+        del list_machine1[1][index];
+        del list_machine2[1][index];
+        W = list_machine1[0] + list_machine2[0];
 
 # concatenation
-print(L2);
-print(L1);
 list_final_ordre = L1 + L2;
 
 # affichage
 print ("tâche : ",list_final_tache);
 print ("ordre : ",list_final_ordre);
-
-
