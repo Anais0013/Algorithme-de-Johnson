f06e7f0090ab4bf4fe5713604bb39d8926f0cb98
Anais0013 <35455501+Anais0013@users.noreply.github.com>
2020-04-20 18:48:30 +0200

    amelioration de l'interface

--- main.py
+++ main.py
@@ -1,47 +1,76 @@
 """
-Realise par : GIANNUZZI Anais
+Réalise par : GIANNUZZI Anais
 Fichier : main.c
-Resume : algorithme de Johnson pour l'ordonnancement minimal
-Date de creation : le 18/04/20
-Date de derniere modification : le 20/04/20
+Résume : algorithme de Johnson pour l'ordonnancement minimal
+Date de création : le 18/04/20
+Date de dernière modification : le 20/04/20
 """
 
+#                   déclaration
 L1 = [];
 L2 = [];
 
-# Saisie des données
-list_machine1 = [[4, 8, 6, 6, 5, 7], [1, 2, 3, 4, 5, 6]];
-list_machine2 = [[3, 6, 6, 6, 8, 4], [1, 2, 3, 4, 5, 6]];
+list_machine1 = [[], []];
+list_machine2 = [[], []];
 
-W = list_machine1[0] + list_machine2[0];
-
-list_final_tache = [1, 2, 3, 4, 5, 6];
+list_final_tache = [];
 list_final_ordre = [];
 
+#                   affichage
+print ("Bonjour tout le monde !\n");
+nombre_de_tache = int(input("Combien il y a de tâches ? "));
+
+i = 0;
+while i < nombre_de_tache :
+#                   remplie les durées pour les deux machines
+    print("Pour la tâche",i+1,",");
+    duree_courante = int(input("Quelle est le temps de la machine 1 ? "));
+    list_machine1[0].append(duree_courante);
+    duree_courante = int(input("Quelle est le temps de la machine 2 ? "));
+    list_machine2[0].append(duree_courante);
+    i = i+1;
+    
+j = 0;
+while j < nombre_de_tache :
+#                   remplie les tâches
+    list_final_tache.append(j+1);
+    list_machine1[1].append(j+1);
+    list_machine2[1].append(j+1);
+    j = j +1
+
+#                   liste de travail
+W = list_machine1[0] + list_machine2[0];
+
 while W != []:
+#                   cherche le minimum
     minimum = min(W);
     if (minimum in list_machine1[0]) :
         index = list_machine1[0].index(minimum);
+#                   garde en mémoire la tâche
         L1.append(list_machine1[1][index]);
-        # on supprime la tache
+#                   supprime
         del list_machine1[0][index];
         del list_machine2[0][index];
         del list_machine1[1][index];
         del list_machine2[1][index];
+#                   nouvelle liste de travail
         W = list_machine1[0] + list_machine2[0];
     else :
         index = list_machine2[0].index(minimum);
+#                   garde en mémoire la tâche
         L2.insert(0, list_machine2[1][index]);
-        # on supprime la tache
+#                   supprime
         del list_machine1[0][index];
         del list_machine2[0][index];
         del list_machine1[1][index];
         del list_machine2[1][index];
+#                   nouvelle liste de travail
         W = list_machine1[0] + list_machine2[0];
 
-# concatenation
+#                   concaténation
 list_final_ordre = L1 + L2;
 
-# affichage
+#                   affichage
+print ("\n");
 print ("tâche : ",list_final_tache);
 print ("ordre : ",list_final_ordre);
